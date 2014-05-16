34de9c0c7d86a3dd44442c4982e9b56ccdbccfd6
Kate <katren4@yandex.ru>
2014-05-16 16:42:28 +0300

    add rank analyse

--- js/methods/rankAnalyse.js
+++ js/methods/rankAnalyse.js
@@ -1,7 +1,19 @@
-define([], function () {
+define(['methods/utils/clone-object'], function (clone) {
 
 	return function(originObjectList){
 		var objectList = clone(originObjectList, []);
+		var averageList = objectList.map(function(object){
+		    return {
+				region: object.region,
+				averageValue: object.reduce(function(a, b){
+				    return a + b;
+				}) / object.length
+			};
+		});
+
+		return averageList.sort(function(a, b){
+		    return a.averageValue > b.averageValue ? -1 : (a.averageValue < b.averageValue ? 1 : 0);
+		});
 	};
 
 });
